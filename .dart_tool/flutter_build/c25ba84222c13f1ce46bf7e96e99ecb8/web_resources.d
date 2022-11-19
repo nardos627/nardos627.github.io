11f136d7fc7de44bf81cc8414381498e0c8e3fa0
Nardos Hadis <nardoshaddis5@gmail.com>
2022-11-19 10:26:05 +0300

    build/main

---
+++ .dart_tool/flutter_build/c25ba84222c13f1ce46bf7e96e99ecb8/web_resources.d
@@ -0,0 +1,1 @@
+ build\\web\\favicon.png build\\web\\icons\\Icon-192.png build\\web\\icons\\Icon-512.png build\\web\\index.html build\\web\\manifest.json:  C:\\travel-app-master\\web\\favicon.png C:\\travel-app-master\\web\\icons\\Icon-192.png C:\\travel-app-master\\web\\icons\\Icon-512.png C:\\travel-app-master\\web\\index.html C:\\travel-app-master\\web\\manifest.json
